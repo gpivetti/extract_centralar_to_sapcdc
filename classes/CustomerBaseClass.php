988722dc7525102a6b35c6a4c872bfea6651c678
Gabriel Pivetti <gapivetti@gmail.com>
2020-07-23 14:58:51 -0300

    higienizado

--- classes/CustomerBaseClass.php
+++ classes/CustomerBaseClass.php
@@ -339,7 +339,6 @@
       $sql = 'update centralar.clientes_cdc set higienizado = "S" where cod_cli = '.$cod_cli;
       $db->query($sql);
       $db->execute();
-      echo 'ERROR: '.$db->error();
     }
 
     public function processingErrorQuery($sqlCustomer, $error, $typeQuery, $cod_cli, $typePerson, $db) {
